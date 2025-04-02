dc2178877a9592543e7421e8e387735437a6c32c
Rus <definitelyrus@gmail.com>
2025-04-02 20:38:45 +0800

    Initial commit
    - Added basic movement
    - Added basic collision
    - Added firing projectiles

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,377 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour {
+
+	#region Physics
+
+	#region Horizontal Movement
+
+	[Header("Horizontal Movement")]
+	public float Acceleration;
+
+	public float MaxVelocity;
+
+	public void MoveListener(bool debug = false) {
+		bool leftInput = Input.GetKey(KeyCode.A);
+		bool rightInput = Input.GetKey(KeyCode.D);
+		if (!leftInput && !rightInput) return;
+
+		string log = "";
+		if (debug) {
+			if (leftInput) log += "L";
+			if (rightInput) log += "R";
+		}
+
+		//Applies the force to the player.
+		float multiplier = Acceleration * Time.fixedDeltaTime;
+		Vector2 leftVelocity = (leftInput ? 1 : 0) * multiplier * Vector2.left;
+		Vector2 rightVelocity = (rightInput ? 1 : 0) * multiplier * Vector2.right;
+		Vector2 velocity = leftVelocity + rightVelocity;
+		Rigidbody.AddForce(velocity, ForceMode2D.Force);
+
+		//Clamps the speed and logs it if debug is enabled.
+		if (debug) {
+			log += $" Velocity: {Rigidbody.linearVelocityX}";
+			bool isClamped = Rigidbody.linearVelocityX > MaxVelocity || Rigidbody.linearVelocityX < -MaxVelocity;
+
+			Rigidbody.linearVelocityX = Mathf.Clamp(Rigidbody.linearVelocityX, -MaxVelocity, MaxVelocity);
+
+			if (isClamped) Debug.Log($"{log} (Clamped to {Rigidbody.linearVelocityX})");
+			else Debug.Log(log);
+		}
+
+		//Clamps the speed without logging it.
+		else Rigidbody.linearVelocityX = Mathf.Clamp(Rigidbody.linearVelocityX, -MaxVelocity, MaxVelocity);
+	}
+
+	#endregion
+
+	#region Jump
+
+	[Header("Jump")]
+	public float InitialJumpForce;
+
+	public float ExtendJumpForce;
+
+	private float extendJumpTimer;
+
+	public float ExtendMinTime;
+
+	public float ExtendMaxTime;
+
+	private bool isJumpQueued;
+
+	private bool isJumping;
+
+	/// <summary>
+	///		If the player is mid-air and inputs a jump a little too early,
+	///		this determines how far off the ground the player can be
+	///		while the input is still considered valid.
+	/// </summary>
+	public float BufferDistance;
+
+	/// <summary>
+	///		Whether the player is currently touching the ground.
+	/// </summary>
+	private bool isGrounded;
+
+	/// <summary>
+	///		How far away the player can be to be considered "on the ground".
+	/// </summary>
+	public float GroundedDistance;
+
+	/// <summary>
+	///		Listens for jump inputs every frame and hands off the
+	///		physics logic to the appropriate methods.
+	/// </summary>
+	/// <param name="debug">Whether to print logs to console.</param>
+	private void JumpListener(bool debug = false) {
+		if (Input.GetKeyDown(KeyCode.Space)) QueueJump(debug);
+
+		//TODO: Allow coyote time
+		bool doGroundedJump = Input.GetKey(KeyCode.Space) && isGrounded;
+		bool jumpOnImpact = isJumpQueued && isGrounded && !isJumping; //Input buffering
+		if (doGroundedJump || jumpOnImpact) Jump(debug);
+
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			if (debug) Debug.Log($"[Player | {name}] Jump released!");
+			isJumpQueued = false;
+			isJumping = false;
+			extendJumpTimer = 0;
+		}
+	}
+
+	/// <summary>
+	///		Checks if the player is close enough to the ground.
+	///		If so, it will make the player jump the moment it touches the ground.
+	/// </summary>
+	/// <param name="debug">Whether to print logs to console.</param>
+	private void QueueJump(bool debug = false) {
+		if (isJumpQueued) return;
+
+		string log = $"[Player | {name}] ";
+
+		//Perform a raycast to check if the player is close enough to the ground.
+		float colliderBaseY = Collider.size.y / 2;
+		float verticalAllowance = colliderBaseY + BufferDistance;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, verticalAllowance, LayerMask.GetMask("Environment"));
+
+		if (hit.collider != null) {
+			isJumpQueued = true;
+			if (debug) log += "Jump queued!";
+		}
+		
+		else if (hit.collider == null && debug) log += "Jump not queued!";
+		if (debug) Debug.Log(log);
+	}
+
+	/// <summary>
+	///		Applies the actual jumping physics logic to the player.
+	/// </summary>
+	/// <param name="debug">Whether to print logs to console.</param>
+	public void Jump(bool debug = false) {
+		float delta = Time.fixedDeltaTime;
+		string log = $"[Player | {name}] ";
+
+		if (debug) log += $" Queued: {isJumpQueued} | Grounded: {isGrounded} | Jumping: {isJumping}";
+
+		//Initial jump. Cannot reactivate until player touches the ground.
+		if (!isJumping) {
+			if (debug) log += $"\nInitial jump!";
+			Rigidbody.AddForce(Vector2.up * InitialJumpForce, ForceMode2D.Impulse);
+			isJumping = true;
+			isGrounded = false;
+			isJumpQueued = false;
+		}
+
+		//Suspended jump.
+		//TODO: Separate this into a different method.
+		else if (isJumping) {
+			//Extend jump time buffer.
+			if (extendJumpTimer < ExtendMinTime) extendJumpTimer += delta;
+
+			//Extending jump.
+			else if (extendJumpTimer < ExtendMaxTime) {
+				extendJumpTimer += delta;
+				Rigidbody.AddForce(delta * ExtendJumpForce * Vector2.up, ForceMode2D.Force);
+
+				if (debug) log = $"\nExtending jump!";
+			}
+
+			//Extend limit reached.
+			else if (extendJumpTimer >= ExtendMaxTime) {
+				if (debug) log = $"\nJump limit reached!";
+				isJumping = false;
+			}
+		}
+
+		if (debug) Debug.Log(log);
+	}
+
+	#endregion
+
+	#endregion
+
+	#region Combat
+
+	#region Aiming
+
+	private Vector2 AimVector;
+
+	public enum AimType { Mouse, Joystick }
+
+	public AimType AimMode;
+
+	public void AimAttack() {
+		//Mouse Input
+		if (AimMode == AimType.Mouse) {
+			//Get the mouse position.
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			//Get the direction to the mouse.
+			AimVector = (mousePosition - (Vector2) transform.position).normalized;
+		}
+
+		//Joystick Input
+		else if (AimMode == AimType.Joystick) {
+			//Use InputSystemPackage 
+			//AimVector = new Vector2(Gamepad.rightStick.x.ReadValue(), Gamepad.rightStick.y.ReadValue()).normalized;
+			//TODO: Handle input modes via a centralized class.
+		}
+	}
+
+	#endregion
+
+	#region Projectiles
+
+	[Header("Attacks")]
+	public GameObject ProjectilePrefab;
+
+	public void AttackListener(bool debug = false) {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			FireProjectile(debug);
+			if (debug) Debug.Log($"[Player | {name}] Attack input received.");
+		}
+	}
+
+	public void FireProjectile(bool debug = false) {
+		//Instantiate a hitObject.
+		GameObject projectile = Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+		Projectile script = projectile.GetComponent<Projectile>();
+		script.Parent = gameObject;
+		script.Fire(AimVector, 1, 1, true);
+
+		if (debug) Debug.Log($"[Player | {name}] Projectile fired!");
+	}
+
+	#endregion
+
+	#region Raycast
+
+	public float LaserRange = 50;
+
+	public float Lifespan = 1;
+
+	public void FireLaser() {
+		#region Raycasts and Effects
+
+		//Perform a raycast scan
+		Vector2 laserDirection = transform.up;
+		RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, laserDirection, LaserRange);
+
+		//For every object hit by the laser beam...
+		foreach (RaycastHit2D hit in hits) {
+
+			//If the object is not this player...
+			if (hit.collider.gameObject != gameObject) {
+				Debug.Log($"[{gameObject.name}] Hit {hit.collider.gameObject.name} at {hit.distance:F2} units.");
+
+				//Explodes where the laser hits
+				//GameObject boom = Instantiate(Explosion, hit.point, transform.rotation);
+				//boom.GetComponent<Explosion>().ExplosionSize = 3;
+
+				//Spawns the laser sprite between the shooter and the hit object
+				//Vector2 deltaGap = Vector2.Lerp(transform.position, hit.centroid, 0.5f);
+				//GameObject laser = Instantiate(LaserSprite, deltaGap, transform.rotation);
+				//laser.transform.localScale = new Vector3(1, hit.distance, 1);
+
+				//If the object is a player, kill it.
+				if (hit.collider.gameObject.GetComponent<Player>() is Player player) player.DoDamage(hit);
+
+				break;
+			}
+
+			//If the object is this player, ignore it.
+			else continue;
+		}
+
+		//If the laser beam reaches its max range...
+		if (hits.Length == 1) {
+			Debug.Log($"[{gameObject.name}] Laser beam reached max range.");
+
+			//Spawns the laser sprite between the shooter and the laser's max range.
+			//Vector2 deltaGap = Vector2.Lerp(transform.position, transform.position + transform.up * LaserRange, 0.5f);
+			//GameObject laser = Instantiate(LaserSprite, deltaGap, transform.rotation);
+			//laser.transform.localScale = new Vector3(1, LaserRange, 1);
+		}
+
+		//Draws the laser beam in the Scene view for debugging.
+		Debug.DrawRay(transform.position, laserDirection * LaserRange, Color.red, 10f);
+
+		#endregion
+	}
+
+	#endregion
+
+	#region Receiving Damage
+
+	[Header("Damage")]
+	public float DamageCooldown = 1;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="hitObject">
+	///		The object that hit the player, usually a projectile.
+	/// </param>
+	public void DoDamage(GameObject hitObject) {
+		//TODO: Implement damage logic.
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="hit">
+	///		The hit point object where the player was hit.
+	/// </param>
+	public void DoDamage(RaycastHit2D hit) {
+		//TODO: Implement damage logic.
+	}
+
+	#endregion
+
+	#endregion
+
+	#region Gameplay Stats
+
+	[Header("Gameplay Stats")]
+	public float Score;
+
+	#endregion
+
+	#region Game Objects and Components
+
+	[Header("Game Objects and Components")]
+	public Rigidbody2D Rigidbody;
+
+	public CapsuleCollider2D Collider;
+
+	public SpriteRenderer Sprite;
+
+	#endregion
+
+	#region Inputs
+
+	//TODO: Receive input keys from a centralized class.
+
+	#endregion
+
+	public void Update() {
+		#region Non-physics Inputs
+
+		AimAttack();
+
+		AttackListener();
+
+		#endregion
+	}
+
+	public void FixedUpdate() {
+
+		#region Checks
+
+		if (!isGrounded) {
+			//Perform a raycast to check if the player is grounded.
+			float colliderBaseY = Collider.size.y / 2;
+			float verticalAllowance = colliderBaseY + GroundedDistance;
+
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, verticalAllowance, LayerMask.GetMask("Environment"));
+
+			if (hit.collider != null) {
+				isGrounded = true;
+				isJumping = false;
+			}
+		}
+
+		#endregion
+
+		#region Physics Inputs
+
+		JumpListener();
+
+		MoveListener();
+
+		#endregion
+	}
+}
---
+++ Assets/Script/Projectile.cs
@@ -0,0 +1,181 @@
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	#region GameObjects and Components
+
+	[Header("GameObjects and Components")]
+	public GameObject Parent;
+
+	private Player ParentScript;
+
+	public Rigidbody2D Body;
+
+	public CircleCollider2D Trigger;
+
+	public CircleCollider2D Collider;
+
+	public TrailRenderer Trail;
+
+	#endregion
+
+	#region Physics
+
+	[Header("Physics")]
+	[Range(0f, 50f)]
+	public float BaseVelocity = 25;
+
+	[Range(0f, 10f)]
+	public float BaseTorque = 5;
+
+	public void Fire(Vector2 direction, float velocityMultiplier = 1, float torqueMultiplier = 1, bool debug = false) {
+		Vector2 forceToAdd = BaseVelocity * velocityMultiplier * direction;
+		float torqueToAdd = BaseTorque * torqueMultiplier;
+
+		Body.AddForce(forceToAdd, ForceMode2D.Impulse);
+		Body.AddTorque(torqueToAdd, ForceMode2D.Impulse);
+
+		if (debug) Debug.Log($"[Projectile] Force: {forceToAdd.magnitude} | Torque: {torqueToAdd}");
+	}
+
+	#endregion
+
+	#region Gameplay Stats
+
+	/// <summary>
+	///		How much impact hitting or missing this projectile
+	///		will have on the player's score.
+	/// </summary>
+	[Header("Gameplay Stats")]
+	public float ScoreWeight;
+
+	public enum ScoreType { Expired, PlayerContact, Environment }
+
+	public void AddScore(ScoreType type, bool debug = false) {
+		float score = 0;
+
+		switch (type) {
+
+			//Projectile despawns itself
+			case ScoreType.Expired:
+				Debug.Log("Projectile expired.");
+				score = ScoreWeight * ExpiredMultiplier;
+				if (debug) Debug.Log($"[Projectile] Expired! Score: {score}");
+				break;
+
+			//Projectile hits player
+			case ScoreType.PlayerContact:
+				Debug.Log("Projectile hit player.");
+				score = ScoreWeight * PlayerContactMultiplier;
+				if (debug) Debug.Log($"[Projectile] Player contact! Score: {score}");
+				break;
+			
+			//Projectile hits environment (usually also subsequently destroyed)
+			case ScoreType.Environment:
+				Debug.Log("Projectile hit environment.");
+				score = ScoreWeight * EnvironmentMultiplier;
+				if (debug) Debug.Log($"[Projectile] Environment contact! Score: {score}");
+				break;
+		}
+
+		ParentScript.Score += score;
+	}
+
+	#endregion
+
+	#region Miscellaneous
+
+	[Header("Miscellaneous")]
+	[Range(0f, 10f)]
+	public float Length;
+
+	[Range(0f, 10f)]
+	public float Lifespan;
+
+	[Range(-10f, 10f)]
+	public float PlayerContactMultiplier = 1;
+
+	[Range(-10f, 10f)]
+	public float ExpiredMultiplier = -0.1f;
+
+	[Range(-10f, 10f)]
+	public float EnvironmentMultiplier = -0.1f;
+
+	public enum CollisionType { PassThrough, Bounce, DestroyOnImpact }
+
+	//UNUSED. Will be limited to DestroyOnImpact for now.
+	public CollisionType playerCollision = CollisionType.DestroyOnImpact;
+
+	public CollisionType environmentCollision = CollisionType.Bounce;
+
+	public void InitializeLongNote(bool debug = false) {
+		//Disable trail when firing short notes.
+		if (Length <= 0) {
+			Trail.enabled = false;
+			if (debug) Debug.Log($"[Projectile] Trail length is 0. Disabling trail.");
+			return;
+		}
+
+		//TODO: Adjust trails n hitboxes n stuff here
+	}
+
+	public void DestroySelf(ScoreType type) {
+		//TODO: Add particle effect.
+		//TODO: Add sound effect.
+
+		AddScore(type);
+		Destroy(gameObject);
+	}
+
+	#endregion
+
+	#region Unity
+
+	void Start()
+    {
+		ParentScript = Parent.GetComponent<Player>();
+		
+		InitializeLongNote();
+
+		//Auto-configure environment collision type.
+		bool shallPassThroughEnvironment = environmentCollision == CollisionType.PassThrough;
+		bool shallBounceOffEnvironment = environmentCollision == CollisionType.Bounce;
+		bool shallImpactWithEnvironment = environmentCollision == CollisionType.DestroyOnImpact;
+
+		if (shallPassThroughEnvironment) Collider.excludeLayers = LayerMask.GetMask("Environment");
+		else if (shallBounceOffEnvironment || shallImpactWithEnvironment) Collider.includeLayers = LayerMask.GetMask("Environment");
+	}
+
+	//For tracking expiration, among others.
+	private void Update() {
+		Lifespan -= Time.deltaTime;
+		if (Lifespan <= 0) DestroySelf(ScoreType.Expired);
+	}
+
+	//For detecting player contact.
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject == Parent) return;
+
+		//If collided with a player...
+		if (collision.gameObject.CompareTag("Player")) {
+			Player player = collision.gameObject.GetComponent<Player>();
+
+			//Add score and maybe destroy self
+			if (playerCollision == CollisionType.DestroyOnImpact) DestroySelf(ScoreType.PlayerContact);
+			else AddScore(ScoreType.PlayerContact);
+
+			//Apply effects to player
+			player.DoDamage(collision.gameObject);
+		}
+	}
+
+	//For detecting environment contact.
+	private void OnCollisionEnter2D(Collision2D collision) {
+		bool isEnvironment = collision.gameObject.CompareTag("Environment");
+		bool destroyOnImpact = environmentCollision == CollisionType.DestroyOnImpact;
+		if (isEnvironment && destroyOnImpact) DestroySelf(ScoreType.Environment);
+	}
+
+	#endregion
+}
